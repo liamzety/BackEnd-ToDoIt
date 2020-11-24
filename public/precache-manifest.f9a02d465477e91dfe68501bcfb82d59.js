953e9fe4978d0407eaad3c720895860bb1a3c735
liamzety <58632095+liamzety@users.noreply.github.com>
2020-11-24 10:19:45 +0200

    build files init

---
+++ public/precache-manifest.f9a02d465477e91dfe68501bcfb82d59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b362ff4dff30cacaa68b7ba6de2532f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b74cd197b791ab5018",
+    "url": "/static/css/main.49b44d1c.chunk.css"
+  },
+  {
+    "revision": "21e1cca6eccae36e6a6e",
+    "url": "/static/js/2.69e5924b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69e5924b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b74cd197b791ab5018",
+    "url": "/static/js/main.b747b9a3.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "98beedb4f25dffc20877000766e919e1",
+    "url": "/static/media/font1-italic.98beedb4.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/static/media/font1-semibold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "686d9685b805c407876cd1abd698fd73",
+    "url": "/static/media/font1.686d9685.ttf"
+  },
+  {
+    "revision": "e30f1374099bd66c39e89fb29e0fd8ce",
+    "url": "/static/media/loader.e30f1374.gif"
+  }
+]);
